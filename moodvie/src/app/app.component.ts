7455310d1a59172cce91be012fa43c3af0c453ab
NeshoPortfolio <kevin.nemec94@gmail.com>
2017-05-30 20:14:03 +0200

    Pushed unecessary stuff

--- moodvie/src/app/app.component.ts
+++ moodvie/src/app/app.component.ts
@@ -8,20 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'app works!';
 
-  n: number;
-
   ngOnInit() {
-    this.n = Math.floor(Math.random() * 3) + 1;
-
-    switch (this.n) {
-      case 1:
-        console.log('Barcelona');
-        break;
-      case 2:
-        console.log('Rhodos');
-        break;
-      case 3:
-      console.log('Makarska');
-    }
   }
 }
